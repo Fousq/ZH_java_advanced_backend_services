2c1175389f7d385d73c3f185e66a62a5e3753c99
Zhanbolat Orakbayev <fous363@gmail.com>
2024-02-07 15:43:55 +0600

    Remove lombok.Data from entity to avoid potential issues due to lombok's generated code

--- src/main/java/com/example/springfoundation/entity/TestObject.java
+++ src/main/java/com/example/springfoundation/entity/TestObject.java
@@ -1,17 +1,41 @@
 package com.example.springfoundation.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
+import java.util.Objects;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "test_table")
-@Data
 public class TestObject {
 
     @Id
     @GeneratedValue
+    @ToString.Include
     private Integer id;
 
     @Column(name = "test_value")
+    @ToString.Include
     private String testValue;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        TestObject that = (TestObject) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
